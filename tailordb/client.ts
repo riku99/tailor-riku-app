1305938c528d6348835b560357e200655561a846
Riku Kurosawa <riku.suny@gmail.com>
2026-01-13 15:06:05 +0900

    fix industry enum

--- tailordb/client.ts
+++ tailordb/client.ts
@@ -1,11 +1,11 @@
 import { db } from "@tailor-platform/sdk";
 
 export const client = db.type("Client", "Client organization", {
+  industry: db.enum(["FINANCE", "RETAIL", "MANUFACTURING", "OTHER"]).description("Industry of the client"),
   name: db.string().description("Client name"),
   country: db.string().description("Country where the client is located"),
   annualRevenue: db.float({ optional: true }).description("Annual revenue in USD"),
   employeeCount: db.int({ optional: true }).description("Number of employees"),
-  industry: db.enum(["FINANCE", "RETAIL", "MANUFACTURING", "OTHER"]).description("Industry of the client"),
   ...db.fields.timestamps(),
 });
 
